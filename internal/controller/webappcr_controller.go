f564f1e65e1667938b6d34b1090f62cd96ffb50f
Shikha Singh <114118576+shikha-commits@users.noreply.github.com>
2023-12-09 10:55:36 -0500

    Update webappcr_controller.go

--- internal/controller/webappcr_controller.go
+++ internal/controller/webappcr_controller.go
@@ -163,7 +163,7 @@ func (r *WebappCRReconciler) Reconcile(ctx context.Context, req ctrl.Request) (c
 							},
 							RestartPolicy: corev1.RestartPolicyOnFailure,
 							ImagePullSecrets: []corev1.LocalObjectReference{
-								{Name: "docker-regcred"},
+								{Name: "webapp-docker-regcred"},
 							},
 						},
 					},
